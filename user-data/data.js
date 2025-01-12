1d503b95da166af11e24ae853a680aff3a908369
Abdelrahman <abdelrhmanelmoghazy1@gmail.com>
2025-01-13 01:36:07 +0300

    about

--- user-data/data.js
+++ user-data/data.js
@@ -6,9 +6,19 @@ export const bio = [
 
 
 export const skills = [
-  "JavaScript, TypeScript, Angular,",
-  "HTML, Bootstrap, CSS, Python, C++, C#, Golang",
-  "GitHub, Debugging, Chrome DevTools"
+  "JavaScript",
+  "TypeScript",
+  "Angular",
+  "HTML",
+  "Bootstrap",
+  "CSS",
+  "Python",
+  "C++",
+  "C#",
+  "Golang",
+  "GitHub",
+  "Debugging",
+  "Chrome DevTools"
 ]
 
 export const experience = [
